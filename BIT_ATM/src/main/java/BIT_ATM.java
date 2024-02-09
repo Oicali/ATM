068beead35e85527e56847e81d14fe5c3274f811
Oicali <121882893+Oicali@users.noreply.github.com>
2024-02-09 13:55:25 +0800

    Added the files needed

---
+++ BIT_ATM/src/main/java/BIT_ATM.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class BIT_ATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ BIT_ATM/src/main/java/defaultFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+//This class creates a default settings for the frames 
+public class defaultFrame extends JFrame {
+    
+    JFrame frame = new JFrame();
+}
